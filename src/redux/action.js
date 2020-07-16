ff9d83adc777892db6c07e16489a651351cff116
oehqoehfo <hosap4ik@gmail.com>
2020-07-16 22:52:48 +0200

    Create action.js

---
+++ src/redux/action.js
@@ -0,0 +1,9 @@
+export let clickAction ="CLICK";
+export const clickFunction = ()=>({
+    type:clickAction
+});
+
+export let rightClickAction ="RIGHT CLICK";
+export const rightClickFunction =()=>({
+    type:rightClickAction
+});
