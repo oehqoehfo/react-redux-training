5cf74d37918af8017bbbb412c3c0587f5ee61f0d
unknown <hosap4ik@gmail.com>
2020-07-16 22:37:19 +0200

    Updates

---
+++ precache-manifest.dac8531fa19192a70d16e3b465c09463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30420cc2985edf1a95fb43f770134cdc",
+    "url": "/react-redux-training/index.html"
+  },
+  {
+    "revision": "348f6f828806a38a570e",
+    "url": "/react-redux-training/static/js/2.7a1d5045.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-training/static/js/2.7a1d5045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161a5c828280580da55d",
+    "url": "/react-redux-training/static/js/main.f1e77bd8.chunk.js"
+  },
+  {
+    "revision": "3751194fa2e9512472e4",
+    "url": "/react-redux-training/static/js/runtime-main.cbd04c83.js"
+  }
+]);
